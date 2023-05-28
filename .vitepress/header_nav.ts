82ca7eef0b2b1328b8b7715c2560cd55769ff9a9
yunumu <1416900346@qq.com>
2023-05-28 21:34:04 +0800

    增加 JavaScript

--- .vitepress/header_nav.ts
+++ .vitepress/header_nav.ts
@@ -3,7 +3,7 @@ export default [
 		text: '精读书籍',
 		link: '/books/Vue.js设计与实现/01_1 ~ 3 章框架设计概览',
 	},
-	{ text: '前端基础', link: '/fe_base/js/01-认识javaScript' },
+	{ text: '前端基础', link: '/fe_base/JavaScript/01-认识javaScript' },
 	{ text: '前端框架', link: '/fe_framework/vue/01-邂逅Vue3' },
 	{ text: '后端基础', link: '/be_base/' },
 	{ text: '后端框架', link: '/be_frame_work/' },
